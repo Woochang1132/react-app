9bb20630eac334c5ae2cb7fdf929dd9d4dc54d99
Woochang1132 <show4031132@gmail.com>
2023-12-10 15:36:36 +0900

    react-app => cleanUp() 함수

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  console.log("i run all the time");
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello() {
   useEffect(() => {
-    console.log("CALL THE API...  ");
+    console.log("created");
+    return () => console.log("destroyed :"); // cleanUp function, component가 destroy될 때 어떤한 행동을 할 수 있도록
   }, []);
-  useEffect(() => {
-    if (keyword !== "") {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when keyword & counter change");
-    // 2개의 keyword중 하나만 바뀌어도 해당 코드 실행될 수 있도록 설정
-  }, [keyword, counter]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="search here"
-      />
-      <h3>{counter}</h3>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
