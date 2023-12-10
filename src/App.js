6948b3431a795194be43e4948d0a83f9cd7b3753
Woochang1132 <show4031132@gmail.com>
2023-12-10 15:07:08 +0900

    react-app => useState의 의미

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
   console.log("i run all the time");
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
     console.log("CALL THE API...  ");
   }, []);
+  useEffect(() => {
+    if (keyword !== "") {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+    // 2개의 keyword중 하나만 바뀌어도 해당 코드 실행될 수 있도록 설정
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back! to react</h1>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="search here"
+      />
+      <h3>{counter}</h3>
       <button onClick={onClick}>click me</button>
     </div>
   );
