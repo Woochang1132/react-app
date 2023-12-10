05ab665640785a3a357b1b8deae0feb3c4892d65
Woochang1132 <show4031132@gmail.com>
2023-12-10 13:31:25 +0900

    react-app > 폴더 및 저장소 변경

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome back! to react</h1>
+      <Button text={"Continue"}></Button>
+    </div>
+  );
+}
+
+export default App;
